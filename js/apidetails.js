40bfdf2a647355779c28f7c8cbf3e6eb5c673f2b
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-15 23:47:57 -0700

    Divided into modules

---
+++ js/apidetails.js
@@ -0,0 +1,15 @@
+var omdbAPI = {
+  "hostname": "http://www.omdbapi.com/",
+  "search": "?s=",
+  "imdbID": "?i=",
+  "plot": "&plot=short",
+  "json": "&r=json",
+  "formatDetailsPath": function(query) {
+    return omdbAPI.imdbID + query.id + omdbAPI.plot + omdbAPI.json;
+  },
+  "formatMoviePath": function(query) {
+    var reYear = /^[1|2][9|0][\d]{2}$/;
+    var search = reYear.test(query.year) ? query.title + "&y=" + query.year : query.title;
+    return omdbAPI.search + search + omdbAPI.plot + omdbAPI.json;
+  }
+};
