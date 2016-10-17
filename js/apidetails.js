cad9002df373b5af4ee7169d6e72620841390e0c
nicolasjhampton <nicolasjhampton@gmail.com>
2016-10-17 16:25:12 -0700

    Adding jsonp

--- js/apidetails.js
+++ js/apidetails.js
@@ -4,12 +4,13 @@ var omdbAPI = {
   "imdbID": "?i=",
   "plot": "&plot=short",
   "json": "&r=json",
+  "callback": "&callback="
   "formatDetailsPath": function(query) {
-    return omdbAPI.imdbID + query.id + omdbAPI.plot + omdbAPI.json;
+    return omdbAPI.imdbID + query.id + omdbAPI.plot + omdbAPI.json + omdbAPI.callback;
   },
   "formatMoviePath": function(query) {
     var reYear = /^[1|2][9|0][\d]{2}$/;
     var search = reYear.test(query.year) ? query.title + "&y=" + query.year : query.title;
-    return omdbAPI.search + search + omdbAPI.plot + omdbAPI.json;
+    return omdbAPI.search + search + omdbAPI.plot + omdbAPI.json + omdbAPI.callback;
   }
 };
